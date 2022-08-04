7d9f5880c612343dac22d539b98dba251bb90443
PRADDZY <64481960+PRADDZY@users.noreply.github.com>
2022-08-04 14:10:14 +0530

    Create getComicPrefix.js

---
+++ funtions/getComicPrefix.js
@@ -0,0 +1,69 @@
+var moment = require("moment");
+var momentDurationFormatSetup = require("moment-duration-format");
+const date = require('date-and-time');
+const { parse } = require("node-html-parser");
+const rp = require("request-promise-native");
+const r = require("request");
+const { MessageEmbed } = require('discord.js');
+const config = require('../config/config.js');
+
+module.exports = {
+
+    async getComic(client, message, comicName, comicYear, firstComicDay, firstComicMonth, embedTitle, embedColor) {
+
+        var archive = `https://gocomics.com/${comicName}/`;
+        var today = new Date();
+        var todayYear = today.getFullYear();
+
+        var firstDay = firstComicDay;
+        var firstMonth = firstComicMonth;
+
+        var month = Math.floor(Math.random() * 12) + 1;
+        let day;
+        if (month === 2) {
+            day = Math.floor(Math.random() * 29) + 1;
+        }
+        if (month === 4 || month === 6 || month === 9 || month === 11) {
+            day = Math.floor(Math.random() * 30) + 1;
+        }
+        if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 ||
+            month === 10 || month === 12) {
+            day = Math.floor(Math.random() * 31) + 1;
+        }
+
+        var randomYear = Math.floor(Math.random() * (todayYear - comicYear) + comicYear);
+        var randomizedMonth = (month < 9 ? '0' : '') + month;
+        var randomizedDay = (day < 9 ? '0' : '') + day;
+
+        if (randomYear < comicYear) randomYear = comicYear;
+        if (randomizedMonth < firstMonth) randomizedMonth = firstMonth;
+        if (randomizedDay < firstDay) randomizedDay = firstDay;
+
+        var url = archive + randomYear + '/' + randomizedMonth + '/' + randomizedDay;
+        if (config.comicUrlLogging === true) console.log(url);
+
+        const parsedPage = parse(await rp(url)
+            .catch(err => {
+                console.log("Request failed\n", err);
+            }));
+
+        let imageURL = parsedPage.querySelector(".item-comic-image img");
+
+        if (imageURL === null) {
+            console.log("No image found");
+            return this.getComic(client, message, comicName, comicYear, firstComicDay, firstComicMonth, embedTitle, embedColor);
+        }
+
+        imageURL = imageURL.rawAttrs.split(/ src=/)[1].replace(/"/g, "");
+
+        const embed = new MessageEmbed()
+            .setColor(embedColor)
+            .setTitle(`${embedTitle}`)
+            .setURL(url)
+            .setImage(imageURL)
+            .setFooter({ text: 'Powered by GoComics.com' })
+            .setTimestamp();
+        message.reply({ embeds: [embed] });
+
+    },
+};

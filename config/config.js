cb4fec5e9f79001f4092f89b6e25af0941d2bf05
PRADDZY <64481960+PRADDZY@users.noreply.github.com>
2022-08-04 14:05:04 +0530

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,37 @@
+var config = {};
+
+config.developers = ["792337080188403723"];
+config.admins = ["976344864792907876"];
+
+config.supportServer = "https://discord.gg/wC2hzB";
+
+config.prefix = "c!";
+
+config.devGuild = "800637252162158602";
+config.clientId = "985786413700251658";
+
+config.autopostLogging = false;
+config.commandLogging = true;
+config.commandLoadLogging = false;
+config.comicUrlLogging = false;
+
+config.colors = {
+    main: "#e67e22",
+    secondary: "#f4511e",
+    error: "#cc0000",
+    success: "#50c878",
+    warning: "#eed202",
+    info: "#226fe6"
+};
+
+config.presence = {
+    status: "online",
+    activities: [
+        {
+            type: "Playing",
+            name: `with ANONYMOUSDUKE`,
+        },
+    ],
+};
+
+module.exports = config;
